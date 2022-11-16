8b1e41586ac6fc69dbac21058314fb88aed19716
olaniyanp11 <107733620+olaniyanp11@users.noreply.github.com>
2022-11-16 12:53:26 -0800

    Create memory.c

---
+++ memory.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+ * bfree - frees a pointer and NULLs the address
+ * @ptr: address of the pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Do
